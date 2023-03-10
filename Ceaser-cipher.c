6da0c5bdfb7df52cee79f3bc6e69647ae0903f80
absurd-rose <you@example.com>
2023-03-10 18:23:48 +0530

    second

--- Ceaser-cipher.c
+++ Ceaser-cipher.c
@@ -3,32 +3,33 @@
 #include<string.h>
 #include<ctype.h>
 #define MAX_SIZE 100
-int encrypt();
+void encrypt();
 void decrypt();
 void clrscr(void);
 int main()
 {
 int c;
 void clrscr();
-/*printf("Enter your choice: \n 1. Encrypt\n 2. Decrypt\n");
-scanf("%d",&c);
-if(c==1)
-{ 
-encrypt();
-}
-else if(c==2)
-{ 
-decrypt();
-}*/
-encrypt();
-decrypt();
+do
+{
+	printf("Enter your choice: \n 1. Encrypt\n 2. Decrypt\n 3. Exit\n");
+	scanf("%d",&c);
+	switch (c)
+	{
+	case 1: encrypt();
+		break;
+	case 2: decrypt();
+		break;
+	}
+} while (c != 3);
 getch();
 }
-int encrypt()
+void encrypt()
 {
 char plaintext[MAX_SIZE],ciphertext[MAX_SIZE];
 int shift = 3, lettercode, len=0, i;
 printf("Enter the string to be encrypted\n");
+fflush(stdin);
 gets(plaintext);
 len = strlen(plaintext);
 for(i=0;i<len;i++)
@@ -61,7 +62,7 @@ gets(ciphertext);
 len = strlen(ciphertext);
 for(i=0;i<len;i++)
 {
-//ciphertext[i] = tolower(ciphertext[i]);
+ciphertext[i] = tolower(ciphertext[i]);
 lettercode = (int)ciphertext[i];
 	if((lettercode>=97)&&(lettercode<=122))
 	{
@@ -76,5 +77,5 @@ lettercode = (int)ciphertext[i];
 	}
 }
 plaintext[i] = '\0';
-printf("Plain text: %s",plaintext);
+printf("Plain text: %s \n",plaintext);
 }
